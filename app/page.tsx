a49c24830b74b419395f7fe990c2e2e514e301f5
Reanz_Arthur <reqbahrf@gmail.com>
2025-12-03 22:52:54 +0800

    chore: Update global CSS and theme variables for dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-      <main className='flex min-h-screen w-full max-w-3xl flex-col justify-center items-center py-32 px-16 bg-white dark:bg-black sm:items-start'>
+    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans'>
+      <main className='flex min-h-screen w-full max-w-3xl flex-col justify-center items-center py-32 px-16 sm:items-start'>
         <div className='flex flex-col justify-center items-center'>
           <h1 className='text-4xl font-bold mb-4 text-center'>
             Track You Money. Even Without Internet.

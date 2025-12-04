831717057aab6dfdbc5cad6e68fdd93bce726f53
Reanz_Arthur <reqbahrf@gmail.com>
2025-12-04 13:29:17 +0800

    feat: Introduce data structures and ActionButton for dashboard features

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -36,6 +36,78 @@ export default function Dashboard() {
     labels: ['Food & Drink', 'Transportation', 'Entertainment', 'Others'],
     color: ['#d91a1a', '#0066ff', '#ff8c00', '#228b22'],
   };
+
+  const exampleRecentActivity = [
+    {
+      activityName: 'StarBucks',
+      category: 'Food & Drinks',
+      date: 'Oct 29',
+      value: '₱ 400',
+    },
+    {
+      activityName: 'Gas Station',
+      category: 'Transportation',
+      date: 'Oct 25',
+      value: '₱ 30',
+    },
+    {
+      activityName: 'Movie Ticket',
+      category: 'Entertainment',
+      date: 'Oct 24',
+      value: '₱ 50',
+    },
+    {
+      activityName: 'Grocery Shopping',
+      category: 'Food & Drinks',
+      date: 'Oct 22',
+      value: '₱ 100',
+    },
+    {
+      activityName: 'Netflix',
+      category: 'Entertainment',
+      date: 'Oct 21',
+      value: '₱ 190',
+    },
+  ];
+
+  const actionBtn = [
+    {
+      btnIcon: <RiAddLine />,
+      btnName: 'Add Expense',
+      btnClass: 'bg-brand-primary text-black',
+    },
+    {
+      btnIcon: <RiReceiptFill />,
+      btnName: 'View All Transactions',
+      btnClass: 'bg-brand-secondary text-white',
+    },
+    {
+      btnIcon: <RiBarChart2Fill />,
+      btnName: 'Detailed Analytics',
+      btnClass: 'bg-brand-secondary text-white',
+    },
+  ];
+
+  interface ActionBtnProps {
+    btnIcon: ReactNode;
+    btnName: string;
+    btnClass: string;
+  }
+
+  const ActionButton: React.FC<ActionBtnProps> = ({
+    btnIcon,
+    btnName,
+    btnClass,
+  }) => {
+    return (
+      <button
+        type='button'
+        className={`${btnClass} w-full flex justify-center items-center py-2 px-4 border border-transparent rounded-2xl shadow-sm text-sm font-medium cursor-pointer`}
+      >
+        <span>{btnIcon}</span>&nbsp;{btnName}
+      </button>
+    );
+  };
   return (
     <div className='md:w-[80dvw] w-full'>
       <div className='mt-9'>

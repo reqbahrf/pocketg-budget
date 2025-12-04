2859e43a61f8281023697295f55f11d75cefd9ee
Reanz_Arthur <reqbahrf@gmail.com>
2025-12-04 11:07:15 +0800

    feat: Implement next-themes for dynamic theme management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { ThemeProvider } from 'next-themes';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
@@ -23,11 +24,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html
+      lang='en'
+      suppressHydrationWarning
+    >
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-emerald-950`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        {children}
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-primary': 'var(--color-brand-primary)',
+        'brand-secondary': 'var(--color-brand-secondary)',
+        'second-dark': 'var(--color-second-dark)',
+        'brand-dark': 'var(--color-brand-dark)',
+        'brand-light': 'var(--color-brand-light)',
+      },
+    },
+  },
+  plugins: [],
+};

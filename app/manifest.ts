8486c35842d79c72181635e706827c60b0a56120
Reanz_Arthur <reqbahrf@gmail.com>
2025-12-10 10:24:00 +0800

    feat: Enhances PWA installability and local development
    Updates the PWA manifest to include a `scope`, an additional icon with `purpose: 'any'`, and screenshots for a more comprehensive installation experience.
    
    Enables HTTPS for the local development server, which is necessary for testing various PWA features.
    
    Adds new screenshot assets for the PWA manifest.
    
    Adjusts padding in the dashboard header for better visual presentation.
    
    Ignores local development configuration files and certificates in version control.

--- app/manifest.ts
+++ app/manifest.ts
@@ -6,7 +6,14 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: 'PocketG',
     description: 'A Progressive Web App Budget Tracker',
     start_url: '/',
+    scope: '/',
     icons: [
+      {
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any',
+      },
       {
         src: '/web-app-manifest-192x192.png',
         sizes: '192x192',
@@ -23,5 +30,18 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#ffffff',
     background_color: '#ffffff',
     display: 'standalone',
+    screenshots: [
+      {
+        src: '/screenshots/desktop.png',
+        sizes: '1897x1058',
+        type: 'image/png',
+        form_factor: 'wide',
+      },
+      {
+        src: '/screenshots/mobile.png',
+        sizes: '1080x2104',
+        type: 'image/png',
+      },
+    ],
   };
 }
--- components/ui/DashboardHeader.tsx
+++ components/ui/DashboardHeader.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function DashboardHeader() {
   return (
     <div className='min-w-dvw md:h-[10dvh] h-[5dvh] flex justify-center'>
-      <div className='md:w-[80dvw] w-full h-full flex justify-between items-center border-b border-gray-600'>
+      <div className='md:w-[80dvw] px-3 py-8 md:py-2 w-full h-full flex justify-between items-center border-b border-gray-600'>
         <div className='flex items-center gap-2 md:gap-4'>
           <Image
             src='/web-app-manifest-192x192.png'

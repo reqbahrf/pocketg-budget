7b8bef430da907d4de302bcdfe300dea6ec343bf
Reanz_Arthur <reqbahrf@gmail.com>
2025-12-11 16:01:29 +0800

    Feat: Add formatDate utility function

---
+++ libs/utils/dateFormatter.ts
@@ -0,0 +1,12 @@
+const formatDate = (dateString: string) => {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true,
+  }).format(new Date(dateString));
+};
+
+export { formatDate };
